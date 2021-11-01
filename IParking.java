362b3e06d1e511ea4ec857a870eb89789883c389
刘宇陶 <2358109408@qq.com>
2021-11-01 20:41:45 +0800

    IParking.java

---
+++ IParking.java
@@ -0,0 +1,19 @@
+package git;
+
+import java.util.ArrayList;
+
+interface IParking {
+    // 打印输出结果
+    public void print();
+    // 检查是否有 carType 对应的停车位
+    // 如果没有空车位，请返回 false ，否则将该车停入车位并返回 true
+    public boolean addCar(int carType);
+    // 解析命令行输入的参数（格式），如文档描述
+public static ParkingSystem parse(String s) throws Exception{
+	String[] ss=s.split(",");
+	ParkingSystem params=new ParkingSystem();
+	params.s=ss;
+	return params;
+ };
+}
+
